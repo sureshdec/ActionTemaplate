ea1123a2038542249fbcc97bd58e45d0fb30f6eb
sureshdec <39115286+sureshdec@users.noreply.github.com>
2022-07-11 15:32:28 +0530

    Update healthCheck.sh

--- .github/actions/healthCheck/healthCheck.sh
+++ .github/actions/healthCheck/healthCheck.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+set -e
 beanstalkStatus=$(aws elasticbeanstalk describe-environment-health --environment-name edl-metastore-api-devl --region us-east-1 --attribute-names HealthStatus | jq '.HealthStatus')
-echo "::set-output name=beanstalkStatus::$beanstalkStatus"
+echo "beanstalkStatus=$beanstalkStatus" >> $GITHUB_ENV 
